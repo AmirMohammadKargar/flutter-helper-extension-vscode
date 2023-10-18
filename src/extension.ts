1ffb6a8c21d5e55fe7860106b84a897c64528df2
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2023-10-18 12:54:23 +0330

    add new command for create directories for features base on clean architecture

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { types, typesWithTemp } from "./constant/temp-types";
+const fs = require("fs");
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -58,6 +59,59 @@ export function activate(context: vscode.ExtensionContext) {
             `Error creating the file: ${error.message}`
           );
         }
+      } else {
+        vscode.window.showInformationMessage(
+          "There is no folder in the workspace"
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
+
+  disposable = vscode.commands.registerCommand(
+    "flutter-helper.createFeatDir",
+    async () => {
+      const pathQuery = await vscode.window.showInputBox({
+        placeHolder:
+          "Write a relative path for your feature like: feature/test/shop",
+        prompt: "Create Feature Directories.",
+      });
+
+      const featureName = await vscode.window.showInputBox({
+        placeHolder: "Write your featureName",
+      });
+
+      if (!pathQuery || !featureName) {
+        vscode.window.showErrorMessage(
+          "A path and filename are mandatory to execute this action"
+        );
+        return;
+      }
+
+      if (vscode.workspace.workspaceFolders) {
+        const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+        const forderPath = vscode.Uri.file(
+          `${wsPath}/${pathQuery}/${featureName}`
+        );
+        try {
+          fs.mkdirSync(forderPath.fsPath);
+        } catch (err) {
+          fs.mkdirSync(`${wsPath}/${pathQuery}`);
+          fs.mkdirSync(forderPath.fsPath);
+        }
+        fs.mkdirSync(forderPath.fsPath + "/domain");
+        fs.mkdirSync(forderPath.fsPath + "/domain/repositories");
+        fs.mkdirSync(forderPath.fsPath + "/domain/usecases");
+        fs.mkdirSync(forderPath.fsPath + "/domain/entities");
+        fs.mkdirSync(forderPath.fsPath + "/data");
+        fs.mkdirSync(forderPath.fsPath + "/data/datasources");
+        fs.mkdirSync(forderPath.fsPath + "/data/repositories");
+        fs.mkdirSync(forderPath.fsPath + "/data/models");
+        fs.mkdirSync(forderPath.fsPath + "/presentation");
+        fs.mkdirSync(forderPath.fsPath + "/presentation/pages");
+        fs.mkdirSync(forderPath.fsPath + "/presentation/managers");
+        fs.mkdirSync(forderPath.fsPath + "/presentation/widgets");
       } else {
         vscode.window.showInformationMessage(
           "There is no folder in the workspace"
