08c0b411190ac906659f6b92aae1d4c09db34724
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2023-10-18 14:21:50 +0330

    add new temp for creating dtp file

--- src/constant/temp-types.ts
+++ src/constant/temp-types.ts
@@ -1,3 +1,4 @@
+import dtoTemp from "../templates/dto.temp";
 import managerTemp from "../templates/manager.temp";
 import remoteDatasourceTemp from "../templates/remote-datasource.temp";
 import repositoryTemp from "../templates/repository.temp";
@@ -6,10 +7,12 @@ export const types = [
   "Repository",
   "Manager (ChangeNotifier)",
   "Remote Datasource",
+  "Dto",
 ];
 
 export const typesWithTemp: { [key: string]: string } = {
   Repository: repositoryTemp,
   "Manager (ChangeNotifier)": managerTemp,
   "Remote Datasource": remoteDatasourceTemp,
+  Dto: dtoTemp,
 };
---
+++ src/templates/dto.temp.ts
@@ -0,0 +1,12 @@
+const dtoTemp = `class Dto {
+ 
+  Dto();
+
+  Map<String, dynamic> toJson() {
+    var params = <String, dynamic>{};
+    return params;
+  }
+}
+`;
+
+export default dtoTemp;
