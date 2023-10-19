21b8a29a517e1ce922ef33b12869d7ebf1e19e29
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2023-10-19 11:05:44 +0330

    add new temp for creating repository implement file

--- src/constant/temp-types.ts
+++ src/constant/temp-types.ts
@@ -1,10 +1,12 @@
 import dtoTemp from "../templates/dto.temp";
 import managerTemp from "../templates/manager.temp";
 import remoteDatasourceTemp from "../templates/remote-datasource.temp";
+import repositoryImplTemp from "../templates/repository-implemment.temp";
 import repositoryTemp from "../templates/repository.temp";
 
 export const types = [
   "Repository",
+  "Repository Implement",
   "Manager (ChangeNotifier)",
   "Remote Datasource",
   "Dto",
@@ -12,6 +14,7 @@ export const types = [
 
 export const typesWithTemp: { [key: string]: string } = {
   Repository: repositoryTemp,
+  "Repository Implement": repositoryImplTemp,
   "Manager (ChangeNotifier)": managerTemp,
   "Remote Datasource": remoteDatasourceTemp,
   Dto: dtoTemp,
---
+++ src/templates/repository-implemment.temp.ts
@@ -0,0 +1,12 @@
+const repositoryImplTemp = `import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: Repository)
+class RepositoryImpl extends Repository {
+  RepositoryImpl();
+
+}
+
+`;
+
+export default repositoryImplTemp;
