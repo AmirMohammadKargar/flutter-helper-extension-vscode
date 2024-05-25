0b5b0448d03826ce7b7074b69fc29a2adf360f38
Amir Mohammad Kargar <a.m.kargar2000@gmail.com>
2024-05-25 13:57:02 +0330

    clean code

--- src/extension.ts
+++ src/extension.ts
@@ -2,125 +2,115 @@ import * as vscode from "vscode";
 import { types, typesWithTemp } from "./constant/temp-types";
 const fs = require("fs");
 
+async function createFile() {
+  const options = types.map((label) => ({
+    label,
+    temp: typesWithTemp[label],
+  }));
+  const tempPicked = await vscode.window.showQuickPick(options, {
+    ignoreFocusOut: true,
+  });
+
+  const pathQuery = await vscode.window.showInputBox({
+    placeHolder: "Write a relative path for your file like: feature/test/shop",
+    prompt: "Create Repository file.",
+  });
+
+  const fileName = await vscode.window.showInputBox({
+    placeHolder: "Write your filename",
+  });
+
+  if (!pathQuery || !fileName) {
+    vscode.window.showErrorMessage(
+      "A path and filename are mandatory to execute this action"
+    );
+    return;
+  }
+
+  if (vscode.workspace.workspaceFolders) {
+    const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const filePath = vscode.Uri.file(`${wsPath}/${pathQuery}/${fileName}.dart`);
+    const wsedit = new vscode.WorkspaceEdit();
+
+    wsedit.createFile(filePath, { ignoreIfExists: false });
+    wsedit.insert(filePath, new vscode.Position(0, 0), tempPicked?.temp || "");
+
+    try {
+      await vscode.workspace.applyEdit(wsedit);
+      vscode.window.showInformationMessage(`Created a new file: ${filePath}`);
+      const doc = await vscode.workspace.openTextDocument(filePath);
+      await vscode.window.showTextDocument(doc);
+    } catch (error) {
+      vscode.window.showErrorMessage(`Error creating the file: ${error}`);
+    }
+  } else {
+    vscode.window.showInformationMessage("There is no folder in the workspace");
+  }
+}
+
+async function createFeatureDirectory() {
+  const pathQuery = await vscode.window.showInputBox({
+    placeHolder:
+      "Write a relative path for your feature like: feature/test/shop",
+    prompt: "Create Feature Directories.",
+  });
+
+  const featureName = await vscode.window.showInputBox({
+    placeHolder: "Write your feature name",
+  });
+
+  if (!pathQuery || !featureName) {
+    vscode.window.showErrorMessage(
+      "A path and feature name are mandatory to execute this action"
+    );
+    return;
+  }
+
+  if (vscode.workspace.workspaceFolders) {
+    const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const featurePath = `${wsPath}/${pathQuery}/${featureName}`;
+    const directories = [
+      "/domain",
+      "/domain/repositories",
+      "/domain/usecases",
+      "/domain/entities",
+      "/data",
+      "/data/datasources",
+      "/data/repositories",
+      "/data/models",
+      "/presentation",
+      "/presentation/pages",
+      "/presentation/managers",
+      "/presentation/widgets",
+    ];
+
+    try {
+      directories.forEach((dir) => {
+        fs.mkdirSync(`${featurePath}${dir}`, { recursive: true });
+      });
+      vscode.window.showInformationMessage(
+        `Feature directories created at: ${featurePath}`
+      );
+    } catch (error) {
+      vscode.window.showErrorMessage(`Error creating directories: ${error}`);
+    }
+  } else {
+    vscode.window.showInformationMessage("There is no folder in the workspace");
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
+  const createFileCommand = vscode.commands.registerCommand(
     "flutter-helper.createFile",
-    async () => {
-      const options = types.map((label) => ({
-        label,
-        temp: typesWithTemp[label],
-      }));
-      const tempPicked = await vscode.window.showQuickPick(options, {
-        ignoreFocusOut: true,
-      });
-
-      const pathQuery = await vscode.window.showInputBox({
-        placeHolder:
-          "Write a relative path for your file like: feature/test/shop",
-        prompt: "Create Repository file.",
-      });
-
-      const fileName = await vscode.window.showInputBox({
-        placeHolder: "Write your filename",
-      });
-
-      if (!pathQuery || !fileName) {
-        vscode.window.showErrorMessage(
-          "A path and filename are mandatory to execute this action"
-        );
-        return;
-      }
-
-      const wsedit = new vscode.WorkspaceEdit();
-
-      if (vscode.workspace.workspaceFolders) {
-        const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        const filePath = vscode.Uri.file(
-          `${wsPath}/${pathQuery}/${fileName}.dart`
-        );
-
-        vscode.window.showInformationMessage(filePath.toString());
-
-        wsedit.createFile(filePath, { ignoreIfExists: false });
-        wsedit.insert(filePath, new vscode.Position(0, 0), tempPicked?.temp!);
-
-        try {
-          await vscode.workspace.applyEdit(wsedit);
-
-          vscode.window.showInformationMessage(
-            `Created a new file: ${filePath}`
-          );
-
-          const openPath = vscode.Uri.file(filePath.path);
-          const doc = await vscode.workspace.openTextDocument(openPath);
-          await vscode.window.showTextDocument(doc);
-        } catch (error: any) {
-          vscode.window.showErrorMessage(
-            `Error creating the file: ${error.message}`
-          );
-        }
-      } else {
-        vscode.window.showInformationMessage(
-          "There is no folder in the workspace"
-        );
-      }
-    }
+    createFile
   );
 
-  context.subscriptions.push(disposable);
-
-  disposable = vscode.commands.registerCommand(
+  const createFeatDirCommand = vscode.commands.registerCommand(
     "flutter-helper.createFeatDir",
-    async () => {
-      const pathQuery = await vscode.window.showInputBox({
-        placeHolder:
-          "Write a relative path for your feature like: feature/test/shop",
-        prompt: "Create Feature Directories.",
-      });
-
-      const featureName = await vscode.window.showInputBox({
-        placeHolder: "Write your featureName",
-      });
-
-      if (!pathQuery || !featureName) {
-        vscode.window.showErrorMessage(
-          "A path and filename are mandatory to execute this action"
-        );
-        return;
-      }
-
-      if (vscode.workspace.workspaceFolders) {
-        const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        const forderPath = vscode.Uri.file(
-          `${wsPath}/${pathQuery}/${featureName}`
-        );
-        try {
-          fs.mkdirSync(forderPath.fsPath);
-        } catch (err) {
-          fs.mkdirSync(`${wsPath}/${pathQuery}`);
-          fs.mkdirSync(forderPath.fsPath);
-        }
-        fs.mkdirSync(forderPath.fsPath + "/domain");
-        fs.mkdirSync(forderPath.fsPath + "/domain/repositories");
-        fs.mkdirSync(forderPath.fsPath + "/domain/usecases");
-        fs.mkdirSync(forderPath.fsPath + "/domain/entities");
-        fs.mkdirSync(forderPath.fsPath + "/data");
-        fs.mkdirSync(forderPath.fsPath + "/data/datasources");
-        fs.mkdirSync(forderPath.fsPath + "/data/repositories");
-        fs.mkdirSync(forderPath.fsPath + "/data/models");
-        fs.mkdirSync(forderPath.fsPath + "/presentation");
-        fs.mkdirSync(forderPath.fsPath + "/presentation/pages");
-        fs.mkdirSync(forderPath.fsPath + "/presentation/managers");
-        fs.mkdirSync(forderPath.fsPath + "/presentation/widgets");
-      } else {
-        vscode.window.showInformationMessage(
-          "There is no folder in the workspace"
-        );
-      }
-    }
+    createFeatureDirectory
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(createFileCommand, createFeatDirCommand);
 }
 
 export function deactivate() {}
